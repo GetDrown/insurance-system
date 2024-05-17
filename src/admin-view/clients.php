79795eb3bb825f0b25272932b70921f972ff909d
Ej <elnesjanisidoro416@gmail.com>
2024-05-18 00:49:40 +0800

    first commit

---
+++ src/admin-view/clients.php
@@ -0,0 +1,66 @@
+<?php include '../../includes/header.php'; ?>
+
+<body>
+    <!-- sidebar -->
+    <div class="basis-2/12 bg-neutral-800 h-dvh flex flex-col justify-start items-center p-3">
+        <div class="h-[150px] mb-[30px]">
+            <img class="h-full" src="../../assets/image/logo.png" alt="">
+        </div>
+        <ul>
+            <li class="mb-[25px] ">
+                <a href="index.php" class="flex w-[230px] hover:bg-green-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/dashboard.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">Dashboard</span>
+                </a>
+            </li>
+            <li class="mb-[25px]">
+                <a href="reports.php" class="flex w-[230px] hover:bg-green-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/reports.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">Reports</span>
+                </a>
+            </li>
+            <li class="mb-[25px]">
+                <a href="clients.php" class="flex w-[230px] hover:bg-green-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/Customer.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">Clients</span>
+                </a>
+            </li>
+            <li class="mb-[25px]">
+                <a href="policy.php" class="flex w-[230px] hover:bg-green-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/policy.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">Policy</span>
+                </a>
+            </li>
+            <li class="mb-[25px]">
+                <a href="sms.php" class="flex w-[230px] hover:bg-green-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/sms.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">SMS</span>
+                </a>
+            </li>
+            <li class="logout">
+                <a href="../../index.php" class="flex w-[230px] hover:bg-red-500 p-3 rounded-lg">
+                    <div class="img-container">
+                        <img src="../../assets/image/logout.png" alt="">
+                    </div>
+                    <span class="ml-3 text-white">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- main-content -->
+    <div class="basis-5/6  h-dvh">
+        <h1 class="">clients</h1>
+    </div>
+</body>
+
+</html>
