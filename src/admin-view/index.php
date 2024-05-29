a4a7c347406f10a18ee92560697fbcb8d7ab404d
Ej <elnesjanisidoro416@gmail.com>
2024-05-29 20:33:27 +0800

    Modified sidebar. Added modal for Client view policy status

--- src/admin-view/index.php
+++ src/admin-view/index.php
@@ -101,15 +101,7 @@ include '../../dbconf/db_config.php';
                     <span class="ml-3 text-white">Clients</span>
                 </a>
             </li>
-            <!-- policcy -->
-            <li class="mb-[25px]">
-                <a href="policy.php" class="flex w-[230px] hover:bg-lime-500 p-3 rounded-lg">
-                    <div class="img-container">
-                        <img src="../../assets/image/policy.png" alt="">
-                    </div>
-                    <span class="ml-3 text-white">Policy</span>
-                </a>
-            </li>
+
             <!-- sms -->
             <li class="mb-[25px]">
                 <a href="sms.php" class="flex w-[230px] hover:bg-lime-500 p-3 rounded-lg">
