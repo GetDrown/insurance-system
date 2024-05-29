a1b8f8ce9f71cd7efece4a8b3f90f69fe30fdf3c
GetDrown <kyledominicp@gmail.com>
2024-05-29 08:31:16 +0800

    added optional $port variable

--- dbconf/db_config.php
+++ dbconf/db_config.php
@@ -4,9 +4,10 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "insurancesystem";
+$port = 3307; //optional when default port is already being used.
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed" . $conn->connect_error);
