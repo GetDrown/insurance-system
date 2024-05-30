6d613593db73dba25fe4cab5d1929c8522420743
Ej <elnesjanisidoro416@gmail.com>
2024-05-30 08:34:07 +0800

    finish reposrt ui

--- src/admin-view/index.php
+++ src/admin-view/index.php
@@ -292,6 +292,7 @@ include '../../dbconf/db_config.php';
         </div>
     </div>
 
+    <script src="../../assets/js/time.js"></script>
     <script>
     const openStaffBtn = document.querySelector("[data-open-staff-modal]");
     const closeStaffBtn = document.querySelector("[data-close-staff-modal]");
@@ -316,28 +317,6 @@ include '../../dbconf/db_config.php';
     closePolicyBtn.addEventListener("click", () => {
         modalPolicy.close();
     });
-
-
-    function updateTime() {
-        var currentDate = new Date();
-        var options = {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        };
-        document.getElementById("currentDate").innerHTML = currentDate.toLocaleDateString('en-US', options);
-        var hours = currentDate.getHours();
-        var minutes = currentDate.getMinutes();
-        var ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12;
-        hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        var currentTime = hours + ':' + minutes + ' ' + ampm;
-        document.getElementById("currentTime").innerHTML = currentTime;
-    }
-    setInterval(updateTime, 1000);
-    updateTime();
     </script>
 
 </body>
