187dc93be1e88e0cbc4a969233220d35e9f9ff03
Ej <elnesjanisidoro416@gmail.com>
2024-05-31 14:18:27 +0800

    message layout 80% done

--- src/admin-view/sms.php
+++ src/admin-view/sms.php
@@ -16,11 +16,11 @@
                 <h1 class="text-[28px] font-semibold text-end" id="currentTime"></h1>
             </div>
         </div>
-        <div class=" h-[280px] grid grid-cols-2 gap-2 ">
-            <div class="bg-white shadow-md  my-2 rounded-md p-2">
+        <div class="h-[280px] grid grid-cols-2 gap-2 overflow-hidden">
+            <div class="bg-white shadow-md my-2 rounded-md p-2">
                 <div class="w-full p-1 border-b h-[42px] bg-white flex justify-between items-center pb-3">
-                    <p class="text-lime-700 font-medium">Users</p>
-                    <div class="border-2 border-lime-600 rounded-md flex justify-between items-center   pr-2">
+                    <p class="text-lime-700 font-medium">Clients</p>
+                    <div class="border-2 border-lime-600 rounded-md flex justify-between items-center pr-2">
                         <div class="bg-lime-600 h-full p-1 text-white">
                             Search Client
                         </div>
@@ -29,20 +29,96 @@
                         <span><i class="fa-solid fa-magnifying-glass text-lime-600"></i></span>
                     </div>
                 </div>
+                <div class="h-[214px] overflow-y-auto clients">
+                    <?php for ($i=0; $i < 7; $i++) { ?>
+                    <div class="bg-white shadow-md mb-3 py-3 px-2 flex justify-between items-center client">
+                        <p class="name text-xs font-medium text-gray-500">JohnDoe <?php echo $i+1; ?></p>
+                        <p class="num text-xs font-medium text-gray-500" id="username">0986 797 2981</p>
+                        <p class="text-xs font-medium  text-gray-500" id="username">Motorcycle Policy</p>
+                        <button
+                            class="selectClientBtn py-1 px-2 text-xs text-green-700 font-semibold border rounded-sm hover:bg-green-600 hover:text-white">Select</button>
+                    </div>
+                    <?php } ?>
+                </div>
             </div>
-            <div class="bg-white shadow-md  my-2 rounded-md p-2">
+
+            <div class="bg-white shadow-md my-2 rounded-md p-2 h-full  overflow-hidden">
                 <div class="w-full p-1 border-b h-[42px] bg-white">
                     <p class="text-lime-700 font-medium">Messaging</p>
                 </div>
+                <form class="h-[214px] grid grid-cols-4 gap-2 p-1  overflow-hidden">
+                    <div class=" col-span-3 p-1">
+                        <div class="flex justify-between items-center">
+                            <label for="client" class="text-xs font-medium text-green-700">Send to:</label>
+                            <input disabled id="clientNameMsg" type="text"
+                                class="focus:outline-none border-b rounded-md">
+                            <input disabled id="clientNumMsg" type="text"
+                                class="focus:outline-none border-b rounded-md">
+                        </div>
+                        <textarea name="msg" id="msg"
+                            class="border w-full mt-3 resize-none focus:outline-none p-1 text-xs text-gray-400"
+                            rows="9"></textarea>
+                    </div>
+
+
+                    <div class="p-1 flex flex-col justify-evenly items-center">
+                        <button class="bg-lime-500 w-[100px] py-2 rounded-md shadow-md">Send</button>
+                        <button class="bg-red-500 w-[100px] py-2 rounded-md shadow-md text-white">Cancel</button>
+                    </div>
+                </form>
             </div>
         </div>
-        <div class="bg-white shadow-md h-[320px] rounded-md overflow-hidden p-2">
+
+        <div class="bg-white shadow-md h-[310px] rounded-md overflow-hidden p-2 mt-2">
             <div class="w-full p-1 border-b h-[42px] bg-white">
                 <p class="text-lime-700 font-medium">History</p>
             </div>
+            <div class="h-[257px] bg-red-400"></div>
         </div>
     </div>
     <script src="../../assets/js/time.js"></script>
+    <script>
+    document.getElementById('searchBar').addEventListener('input', function() {
+        var searchTerm = this.value.toLowerCase();
+        var clients = document.querySelectorAll('.client');
+        var hasVisibleClients = false;
+
+        clients.forEach(function(client) {
+            var username = client.querySelector('.username').textContent.toLowerCase();
+            if (username.includes(searchTerm)) {
+                client.classList.remove('hidden');
+                hasVisibleClients = true;
+            } else {
+                client.classList.add('hidden');
+            }
+        });
+
+        // document.getElementById('noClientsMessage').classList.toggle('hidden', hasVisibleClients);
+        var element = document.getElementById('noClientsMessage');
+
+        if (!hasVisibleClients) {
+            element.classList.remove('hidden');
+            element.classList.add('flex', 'items-center', 'justify-center');
+        } else {
+            element.classList.remove('flex', 'items-center', 'justify-center');
+            element.classList.add('hidden');
+        }
+
+    });
+
+    const selectClientBtn = document.querySelectorAll(".selectClientBtn")
+    const clientNameMsg = document.getElementById("clientNameMsg");
+    const clientNumMsg = document.getElementById("clientNumMsg");
+
+    selectClientBtn.forEach(btn => {
+        btn.addEventListener("click", function() {
+            const clientsDiv = btn.closest(".clients");
+            const name = clientsDiv.querySelector('.name').textContent;
+            const num = clientsDiv.querySelector('.num').textContent;
+            console.log(name + num);
+        });
+    });
+    </script>
 </body>
 
 </html>
