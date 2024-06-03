80723e9fa204ee1cb92add0c668edc5be977cf0c
GetDrown <kyledominicp@gmail.com>
2024-06-03 15:20:58 +0800

    added function that fetches the customer based on his customer ID

--- src/admin-view/nonlifepolicy.php
+++ src/admin-view/nonlifepolicy.php
@@ -16,8 +16,11 @@ if ($customer_id) {
     if ($result->num_rows > 0) {
         $customer = $result->fetch_assoc();
     }
+   
 }
+
 ?>
+
 
 <body class="bg-gray-300">
     <!-- sidebar -->
@@ -29,7 +32,9 @@ if ($customer_id) {
             <!-- <label for="" class="mb-2 text-[14px]">Step 2</label> -->
 
 
-            <form action="" method="POST " class="flex flex-col ">
+            <form action="../../phpscript/addnonlife.php" method="POST" class="flex flex-col ">
+                <input type="hidden" name="customer_id" value="<?php echo htmlspecialchars($customer_id); ?>">
+
                 <div class="mt-4 flex mb-[25px] space-x-4">
                     <label class="text-3xl ml-2 font-poppins text-lime-700">Non-Life Insurance Policy Application</label>
                     <select name="non_life_selection" id="" class="rounded-md px-[6px] py-[7px] bg-white border-[3px]">
@@ -53,21 +58,21 @@ if ($customer_id) {
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Business Profession</label>
-                        <input type="text" placeholder="Business Profession" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="business_prof" type="text" placeholder="Business Profession" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Date Issued</label>
-                        <input type="date" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="date_issued" type="date" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Confirmation of Cover No.</label>
-                        <input type="text" placeholder="Confirmation of Cover No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="conf_cover" type="text" placeholder="Confirmation of Cover No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Official Receipt No.</label>
-                        <input type="text" placeholder="Official Receipt No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="official_receipt" type="text" placeholder="Official Receipt No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col">
                         <label for="" class="font-poppins">Period of Issuance</label>
-                        <input type="date" class="mt-2 mb-[8px] rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="from_issuance" type="date" class="mt-2 mb-[8px] rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">To</label>
-                        <input type="date" class="mt-2 rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="to_issuance" type="date" class="mt-2 rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                 </div>
 
@@ -78,33 +83,33 @@ if ($customer_id) {
                 <div class="flex flex-row ml-[10px] md:ml-[7px] space-x-4 mb-[25px]">
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Model</label>
-                        <input type="text" placeholder="Model" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_model" type="text" placeholder="Model" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Plate No.</label>
-                        <input type="text" placeholder="Plate No." class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_platenum" type="text" placeholder="Plate No." class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Make</label>
-                        <input type="text" placeholder="Make" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_make" type="text" placeholder="Make" class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Serial/Chassis No.</label>
-                        <input type="text" placeholder="Serial/Chassis No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_chassis" type="text" placeholder="Serial/Chassis No." class="rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Type of Body</label>
-                        <input type="text" placeholder="Type of Body" class="rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_type" type="text" placeholder="Type of Body" class="rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Motor No.</label>
-                        <input type="text" placeholder="Motor No." class="rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_motornum" type="text" placeholder="Motor No." class="rounded-md w-[150px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Color</label>
-                        <input type="text" placeholder="Color" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_color" type="text" placeholder="Color" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Authorized Capacity</label>
-                        <input type="text" placeholder="Authorized Capacity" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_auth" type="text" placeholder="Authorized Capacity" class="rounded-md w-[200px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                     <div class="flex flex-col space-y-2">
                         <label for="" class="font-poppins">Blt File No.</label>
-                        <input type="text" placeholder="Blt File No." class="rounded-md w-[170px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_blt" type="text" placeholder="Blt File No." class="rounded-md w-[170px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                         <label for="" class="font-poppins">Unladen Weight (kgs.)</label>
-                        <input type="text" placeholder="Motor No." class="rounded-md w-[170px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                        <input name="vehicle_weight" type="text" placeholder="Motor No." class="rounded-md w-[170px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                     </div>
                 </div>
 
@@ -123,7 +128,7 @@ if ($customer_id) {
                         <div class="flex flex-row justify-between space-x-4 ">
                             <label for="" class="font-poppins mt-[5px] mr-[15px]">Premium Paid</label>
                             <img src="../../assets/image/peso.png" alt="" class="w-[20px] h-[20px] mt-[4px]">
-                            <input type="text" placeholder="100,000.00" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                            <input name="vehicle_paid" type="text" placeholder="100,000.00" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
 
                         </div>
                     </div>
@@ -174,7 +179,7 @@ if ($customer_id) {
                         <div class="flex flex-row ">
                             <label for="" class="font-poppins mt-[3px] mr-[70px] font-semibold">Total</label>
                             <img src="../../assets/image/peso.png" alt="" class="w-[20px] h-[20px] mt-[5px]">
-                            <input type="text" placeholder="" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-black placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500" disabled>
+                            <input name="vehicle_totalpaid" type="text" placeholder="" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-black placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500" >
                         </div>
                     </div>
                 </div>
@@ -182,10 +187,10 @@ if ($customer_id) {
                 <div class="flex flex-row ml-[7px]">
                     <label for="" class="font-poppins mt-[3px] mr-[8px]">Forms and Endorsements Made Part of this Policy
                         at time of Issue</label>
-                    <input type="text" placeholder="" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-black placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
+                    <input name="form_endorsement" type="text" placeholder="" class=" rounded-md w-[250px] py-[2px] px-[4px] placeholder:pl-[2px] placeholder:text-black placeholder:text-sm bg-white border-2 border-slate-300 focus:ring-1 focus:outline-none focus:border-lime-500 focus:ring-lime-500">
                 </div>
                 <div class="flex flex-row justify-end space-x-4 mr-7 mt-4">
-                    <button class="rounded-md px-[6px] py-[9px] bg-lime-500 hover:bg-lime-600  transition ease-in-out duration-300">Submit
+                    <button type="submit" class="rounded-md px-[6px] py-[9px] bg-lime-500 hover:bg-lime-600  transition ease-in-out duration-300">Submit
                         Application</button>
                     <button class="rounded-md px-[14px] py-[9px] bg-red-300 ">Cancel</button>
                 </div>
