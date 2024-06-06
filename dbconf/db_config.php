739e8ccd443cc91e40d8634a512d9294172e6a4f
GetDrown <kyledominicp@gmail.com>
2024-06-06 16:53:01 +0800

    optional ports

--- dbconf/db_config.php
+++ dbconf/db_config.php
@@ -4,10 +4,10 @@ $servername = "localhost"; //optional to add port number of the mysql server
 $username = "root";
 $password = "";
 $dbname = "insurancesystem";
-$port = 3307; //optional when default port is already being used.
+// $port = 3307; //optional when default port is already being used.
 
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed" . $conn->connect_error);
